468d5cab67a9bbd38f978f80eef041eabb091b9e
nooty <annsk5342@gmail.com>
2023-04-27 02:04:42 +0900

    recyclerview 추가했음

--- app/src/main/java/com/example/chatingtest/Chat_Activity.java
+++ app/src/main/java/com/example/chatingtest/Chat_Activity.java
@@ -1,17 +1,39 @@
 package com.example.chatingtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Chat_Activity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private chatingAdapter adapter;
+
+    private ArrayList<user_chat_item> chatActivityDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        recyclerView = findViewById(R.id.chat_recyclerview);
 
+        adapter = new chatingAdapter();
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        chatActivityDataList = new ArrayList<>();
+
+        chatActivityDataList.add(new user_chat_item("nooty","안녕"));
+
+        adapter.setAdapterChatList(chatActivityDataList);
 
     }
 }
