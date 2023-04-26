3941c03327a8e14d67c0bcb2e2a0e3910fdcca0c
nooty <annsk5342@gmail.com>
2023-04-27 01:05:51 +0900

    edittext 만들고 recyclerview 넣기만 함

--- app/src/main/java/com/example/chatingtest/Chat_Activity.java
+++ app/src/main/java/com/example/chatingtest/Chat_Activity.java
@@ -10,5 +10,8 @@ public class Chat_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+
     }
 }
